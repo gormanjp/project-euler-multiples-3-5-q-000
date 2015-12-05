8892f08a0d3b1217cb8da740c9fde5f3331721ee
gormanjp <jpgorman8@gmail.com>
2015-12-05 22:33:50 +0000

    nitrous-backup at Sat Dec  5 22:33:50 UTC 2015

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+i = 0
+
+def collect_multiples(sum)
+   while sum<1000
+    if i % 3 == 0
+      sum += i
+    elsif i % 5 == 0
+      sum += i
+    end
+    i += 1
+  end
+end
+puts sum
